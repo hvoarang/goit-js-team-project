2d54c2a2216c47651d79962acc4c0a9d4072baa4
Yen Maryna <113536885+6615marik@users.noreply.github.com>
2023-01-02 20:24:36 +0200

    d

---
+++ src/header_js/search-form.js
@@ -0,0 +1,3 @@
+// чтобы найти форму  для моб меню и в хедере есть два классы(но там разные названия) и id (тут поиск по id)
+const searchForm = document.querySelector('#search-form');
+searchForm.addEventListener('submit', eventSearch);
