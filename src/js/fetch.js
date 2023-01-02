4e8e757085d8e4b60bfd269ae71a780db67987f0
Nataliia Shkarupeta <natalinardi.kh@gmail.com>
2023-01-02 18:25:39 +0200

    add import script

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -3,7 +3,7 @@ export const url_by_first_letter = `https://thecocktaildb.com/api/json/v1/1/sear
 export const url_ingredient_by_name = `https://thecocktaildb.com/api/json/v1/1/search.php?i=`;
 
 export function fetchApi(url, el) {
-  fetch(`${url}${el}`).then(response => {
+ return fetch(`${url}${el}`).then(response => {
     return response.json();
   });
 }
