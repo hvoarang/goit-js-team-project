61f8e3370274b236689d34360f53e11fc21dd3a3
Denys iElkin <hvoarang@gmail.com>
2023-01-03 17:13:18 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './js/fetch';
 import './js/hero-select-m';
 import './js/hero';
-import './js/open_modal.js';
-import './js/search_header.js';
+import './js/open_modal';
+import './js/search_header';
